cf61cf0ffdda2960416093cffe02134a05f89610
batlicka <vojta@vagunda.eu>
2020-05-02 06:46:47 +0200

    added handler "handleGoToRecipeDetailPage"

--- src/components/organisms/RecipeList.js
+++ src/components/organisms/RecipeList.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Box from "../atoms/Box";
 import RecipeListItem from "../organisms/RecipeListItem";
+import { useHistory } from "react-router-dom";
 
 const RecipeList = ({ recipes }) => {
+  const { push } = useHistory();
+
+  // const handleGoToRecipeDetailPage = recipeID => push(`/recipes/${recipeID}`);
+  const handleGoToRecipeDetailPage = (recipeID) => {
+    console.log(`card with ${recipeID} was clicked`);
+  };
+
   return recipes.map((recipe) => (
     <Box key={recipe._id}>
-      <RecipeListItem recipe={recipe} />
+      <RecipeListItem
+        recipe={recipe}
+        onClick={() => handleGoToRecipeDetailPage(recipe._id)}
+      />
     </Box>
   ));
 };
