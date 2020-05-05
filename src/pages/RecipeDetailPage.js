bd5105145f371fac4dc535931ea4d8f9d76a523f
tomesfilip <60915718+tomesfilip@users.noreply.github.com>
2020-05-05 15:14:50 +0200

    Add files via upload

--- src/pages/RecipeDetailPage.js
+++ src/pages/RecipeDetailPage.js
@@ -2,31 +2,74 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import styled from "@emotion/styled";
 
+import { faClock } from '@fortawesome/free-solid-svg-icons';
+
 import useRecipeDetail from "../hooks/useRecipeDetail";
+import useRecipeIngredients from "../hooks/useRecipeIngredients";
 import Layout from "../components/organisms/Layout";
 import RichText from "../components/molecules/RichText";
 
-const RightTable = styled.div`
-  padding-right: 16px;
-  flex: 1 1 0%;
-  box-sizing: border-box;
+
+
+const RecipeDetailContainer = styled.div`
   display: flex;
-  width: 60%;
+  flex-direction: row;
+  flex-wrap: wrap;
+  box-sizing: border-box;
+  padding: 1rem;
+  align-items: center;
+  
 `;
+
+const RecipeDetailHeaderFooter = styled.div`
+  flex: 1 1 100%;
+  margin-bottom: 1rem;
+`;
+
+const IngredientsItem = styled.div`
+  text-align: center;
+  flex: 1 1 300px;
+  display: flex;
+  //padding-right: 1rem;
+  box-sizing: border-box;
+`;
+
+
+const DirectionsItem = styled.div`
+  text-align: left;
+  flex: 1 1 600px;
+  box-sizing: border-box;
+`;
+
 
 const RecipeDetailPage = () => {
   const recipeID = useParams();
   const recipeDetail = useRecipeDetail(recipeID);
-  console.log("recipe ingredients");
-  console.log(recipeDetail.recipeDetail.ingredients);
+
+  
+
+  const {preparationTime, title, lastModifiedDate, directions,ingredients} = recipeDetail.recipeDetail;
+  console.log(ingredients);
 
   return (
     <Layout>
-      <h1>{recipeDetail.recipeDetail.title}</h1>
-      <p>{recipeDetail.recipeDetail.preparationTime}</p>
-      <RightTable>
-        <RichText text={recipeDetail.recipeDetail.directions} />
-      </RightTable>
+      <RecipeDetailContainer>
+        <RecipeDetailHeaderFooter>
+          <h1>{title}</h1>
+          <p>⏲️{preparationTime} minut</p>
+        </RecipeDetailHeaderFooter>
+        <IngredientsItem>
+          <h2>Ingrediencie:</h2>
+          <p>{}</p>
+        </IngredientsItem>
+        <DirectionsItem>
+          <h2>Postup:</h2>
+          <RichText text={directions} />
+        </DirectionsItem>
+        <p>Posledná změna:{lastModifiedDate}</p>
+      </RecipeDetailContainer>
+      
+
     </Layout>
   );
 };
