f35da1f769c15a700607ca053b43b5e5fcb6e001
Petr Jelínek <petulinek.jel@gmail.com>
2020-05-06 08:59:21 +0200

    Deleting item from the list

--- src/pages/RecipeDetailPage.js
+++ src/pages/RecipeDetailPage.js
@@ -1,17 +1,31 @@
-import React, { useState } from "react";
-import {
-  RecipeDetailContainer,
-} from "../styles/styles";
+import React from "react";
+import axios from "axios";
+import { useParams, useHistory } from "react-router-dom";
+import { RecipeDetailContainer } from "../styles/styles";
 import Layout from "../components/organisms/Layout";
 import RecipeDetailListItem from "../components/organisms/RecipeDetailListItem";
 
 const RecipeDetailPage = () => {
+  const recipeID = useParams();
 
+  const url = `https://exercise.cngroup.dk/api/recipes/${recipeID.recipeID}`;
+  const { push } = useHistory();
+
+  const removeItem = async () => {
+    try {
+      const result = await axios.delete(url);
+      push(`/`);
+    } catch ({ message }) {
+      console.log(message);
+    }
+  };
 
   return (
     <Layout>
       <RecipeDetailContainer>
-        <RecipeDetailListItem></RecipeDetailListItem>
+        <RecipeDetailListItem
+          onRemoveClicked={() => removeItem()}
+        ></RecipeDetailListItem>
       </RecipeDetailContainer>
     </Layout>
   );
