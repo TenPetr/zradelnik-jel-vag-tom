53e2f3076af38b9fc516f1fc34bd5b05f713fc15
tomesfilip <60915718+tomesfilip@users.noreply.github.com>
2020-05-06 08:46:11 +0200

    bug fix

--- src/pages/RecipeDetailPage.js
+++ src/pages/RecipeDetailPage.js
@@ -1,49 +1,17 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
 import {
   RecipeDetailContainer,
-  RecipeDetailHeaderFooter,
-  IngredientsItem,
-  DirectionsItem,
 } from "../styles/styles";
-import useRecipeDetail from "../hooks/useRecipeDetail";
-import useRecipeIngredients from "../hooks/useRecipeIngredients";
 import Layout from "../components/organisms/Layout";
-import RichText from "../components/molecules/RichText";
+import RecipeDetailListItem from "../components/organisms/RecipeDetailListItem";
 
 const RecipeDetailPage = () => {
-  const recipeID = useParams();
-  const recipeDetail = useRecipeDetail(recipeID);
 
-  const {
-    preparationTime,
-    title,
-    lastModifiedDate,
-    directions,
-    ingredients,
-  } = recipeDetail.recipeDetail;
-  console.log(ingredients);
 
   return (
     <Layout>
       <RecipeDetailContainer>
-        <RecipeDetailHeaderFooter>
-          <h1>{title}</h1>
-          <p>
-            <span role="img" aria-label="clock">
-              ⏲️
-            </span>
-            {preparationTime} minut
-          </p>
-        </RecipeDetailHeaderFooter>
-        <IngredientsItem>
-          <h2>Ingrediencie:</h2>
-        </IngredientsItem>
-        <DirectionsItem>
-          <h2>Postup:</h2>
-          <RichText text={directions} />
-        </DirectionsItem>
-        <p>Posledná změna:{lastModifiedDate}</p>
+        <RecipeDetailListItem></RecipeDetailListItem>
       </RecipeDetailContainer>
     </Layout>
   );
