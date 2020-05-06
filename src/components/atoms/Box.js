c9e1082db7339fae5dccb481f2ef6c34f5afe85a
batlicka <vojta@vagunda.eu>
2020-05-06 09:38:20 +0200

    edited Box

--- src/components/atoms/Box.js
+++ src/components/atoms/Box.js
@@ -1,5 +1,5 @@
 import styled from "@emotion/styled";
-
+import img from '../../../public/food-placeholder.png';
 const Box = styled.div({
   display: "flex",
   border: "1px solid white",
@@ -9,6 +9,7 @@ const Box = styled.div({
   textAlign: "center",
   padding: "15px",
   backgroundSize: "cover",
+  backgroundImage: url(${img})
 });
 
 export default Box;
