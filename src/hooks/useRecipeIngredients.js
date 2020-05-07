047afb7ec0faa9af649f090e2b4738f29e8fc964
tomesfilip <60915718+tomesfilip@users.noreply.github.com>
2020-05-07 23:02:14 +0200

    detailListItemEdit changes

--- src/hooks/useRecipeIngredients.js
+++ src/hooks/useRecipeIngredients.js
@@ -3,12 +3,12 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const useRecipeIngredients = ({}) => {
-  const url = "https://exercise.cngroup.dk/api/recipes/ingredients/";
+  const url = `https://exercise.cngroup.dk/api/recipes/ingredients/`;
   const [ingredients, setIngredients] = useState({ ingredients: [], error: "" });
 
   useEffect(() => {
     fetchRecipes();
-  }, );
+  }, []);
 
   const fetchRecipes = async () => {
     try {
